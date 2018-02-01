2fe715110271ab0c2f4a6b74b0066fba5e94aadd
JakeMDuthie <jakeduthie@rocketmail.com>
2018-02-01 13:55:42 +0000

    Minor Updates
    Model/Material Imports
    UI Changes
    Scripts Added:
    - Camera Controller
    - State Manager

---
+++ PettyKings_02/Assets/Scripts/StateManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+
+    enum GAMESTATE {
+        MENU,
+        STAGEONE,
+        STAGETWO,
+        STAGETHREE,
+        GAMEOVER
+    };
+
+    GAMESTATE CurrentState_;
+
+	// Use this for initialization
+	void Start () {
+        CurrentState_ = GAMESTATE.MENU;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ReturnToMenu()
+    {
+        CurrentState_ = GAMESTATE.MENU;
+    }
+}
