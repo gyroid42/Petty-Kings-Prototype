0828c843bbce080b3e0e38623dc115d1b6a91313
cc3397 <hotsalsalove@gmail.com>
2018-02-05 17:38:23 +0000

    Terrain and Building Stage
    -Added a terrain to the scene, replacing the ground that was there
    previosly
    -Changed the groundTile prefab to use a shader instead of a red or green
    colour
    -Added a shader file for the groundTile prefab, located in
    Assets/Shaders
    -Added a button to UI for testing purposes
    -Added a new script that allows player to drag and drop gameobjects from
    UI into the world, located at Assets/Scripts/Enviroment
    -Added a cube that sits under the world for testing purposes
    -Added in the grass texture and applied it to terrain, texture is
    located at Assets/Materials/Textures

--- PettyKings_02/Assets/Scripts/Enviroment/GroundTileMesh.cs
+++ PettyKings_02/Assets/Scripts/Enviroment/GroundTileMesh.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 
 public class GroundTileMesh : MonoBehaviour {
 
-    public float tileWidth = 1; //width of ground tile  
-    public float tileLength = 1; //length on ground tile
+ 
     public bool isWalkable; //can the player walk on this ground tile 
     public Material materialGreen;
     public Material materialRed;
@@ -18,15 +17,37 @@ public class GroundTileMesh : MonoBehaviour {
         Vector3[] vertices = new Vector3[4];
 
         //define vertices
-        vertices[0] = new Vector3(-tileWidth, 0, -tileLength);
-        vertices[1] = new Vector3(-tileWidth, 0, tileLength);
-        vertices[2] = new Vector3(tileWidth, 0, tileLength);
-        vertices[3] = new Vector3(tileWidth, 0, -tileLength);
+        vertices[0] = new Vector3(-1, 0, -1);
+        vertices[1] = new Vector3(-1, 0, 1);
+        vertices[2] = new Vector3(1, 0, 1);
+        vertices[3] = new Vector3(1, 0, -1);
 
         //assign the vertices to the mesh
         mesh.vertices = vertices;
+        
+        //define UV's
+        Vector2[] uv = new Vector2[4];
+
+        uv[0] = new Vector2(0, 0);
+        uv[1] = new Vector2(1, 0);
+        uv[2] = new Vector2(0, 1);
+        uv[3] = new Vector2(1, 1);
+
+        mesh.uv = uv;
+
+        //define normals
+        Vector3[] normals = new Vector3[4];
+
+        normals[0] = Vector3.up;
+        normals[1] = Vector3.up;
+        normals[2] = Vector3.up;
+        normals[3] = Vector3.up;
+
+        mesh.normals = normals;
+
         //define traingle order
         mesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+
 
         //assign coded mesh to game object
         GetComponent<MeshFilter>().mesh = mesh;
@@ -39,6 +60,23 @@ public class GroundTileMesh : MonoBehaviour {
         {
             GetComponent<Renderer>().material = materialGreen;
         }
-	}
+
+        //set thickness of highlight to 0 at run time, this allows for easier editing
+        //GetComponent<Renderer>().material.SetFloat("_Thickness", 0.0f);
+    }
+
+    //**COMMENTED OUT FOR FURTHER DESIGN DISCUSSIONS**//
+
+    /*private void OnMouseOver() //highlight the tile when mouse is over the tile
+    {
+        
+        GetComponent<Renderer>().material.SetFloat("_Thickness", 4.0f);
+        
+    }
+
+    private void OnMouseExit() //un-highlight tile when mouse leaves the tile
+    {
+        GetComponent<Renderer>().material.SetFloat("_Thickness", 0.0f);
+    }*/
 
 }
