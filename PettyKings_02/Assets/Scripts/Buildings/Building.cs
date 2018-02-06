c7ffefd98769217e161137c3afab97aa5e843eb0
Jock MacDonald <1501651@abertay.ac.uk>
2018-02-06 18:32:08 +0000

    added new scriptableObject for buildings
    buildings contain:
    -name
    -artwork (UI)
    -gameObject (in game Model)
    -building cost (food, wood, men)
    
    hotfix:
    -fixed issue with print method in the event object printing the wrong name

---
+++ PettyKings_02/Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Building", menuName = "Building")]
+public class Building : ScriptableObject {
+
+    // All data required for a building
+
+    public string name_;
+    public Texture artwork_;
+    public GameObject buildingModel_;
+
+    public int foodCost_;
+    public int woodCost_;
+    public int menCost_;
+
+
+
+    public void Print()
+    {
+        Debug.Log(name_);
+    }
+
+
+    public int[] GetCost()
+    {
+        int[] resources = new int[3] { foodCost_, woodCost_, menCost_ };
+        return resources;
+    }
+}
