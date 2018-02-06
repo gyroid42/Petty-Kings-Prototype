ef27107c6e19a10d45388531cfe271d41f344eab
Jock MacDonald <1501651@abertay.ac.uk>
2018-02-06 19:26:22 +0000

    Added building controller
    -Will update UI elements with the values from a building scriptable object
    - has purchase method to purchase building using the resources the player has

--- PettyKings_02/Assets/Scripts/Buildings/Building.cs
+++ PettyKings_02/Assets/Scripts/Buildings/Building.cs
@@ -2,30 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "New Building", menuName = "Building")]
 public class Building : ScriptableObject {
 
     // All data required for a building
 
+    // Information about building
     public string name_;
-    public Texture artwork_;
+    public Sprite artwork_;
     public GameObject buildingModel_;
 
+    // Cost of the building
     public int foodCost_;
     public int woodCost_;
     public int menCost_;
 
 
 
+    // Print method for debugging
     public void Print()
     {
         Debug.Log(name_);
     }
 
 
+    // Returns the cost of the object
     public int[] GetCost()
     {
+        // Store costs in an array then return it
         int[] resources = new int[3] { foodCost_, woodCost_, menCost_ };
+        return resources;
+    }
+
+    // Returns the cost of the object with negative values
+    public int[] GetCostNegative()
+    {
+        // Store costs in an array then return it
+        int[] resources = new int[3] { -foodCost_, -woodCost_, -menCost_ };
         return resources;
     }
 }
