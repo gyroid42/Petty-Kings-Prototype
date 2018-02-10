dfa0a93dc6e8f30359917dbe944868123f3083da
Jock MacDonald <1501651@abertay.ac.uk>
2018-02-10 21:37:51 +0000

    Added tabs to Building UI
    -Added tabs for Units and Buildings
    
    all buttons now enlarge when hovered over

---
+++ PettyKings_02/Assets/Scripts/UI Scripts/ConstructionDisplayManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DisplayPanel
+{
+    NONE,
+    BUILDINGS,
+    UNITS
+}
+
+
+public class ConstructionDisplayManager : MonoBehaviour {
+
+    
+
+    //private DisplayPanel currentPanel_;
+
+
+    public GameObject buildingsPanel_;
+    public GameObject unitsPanel_;
+    public GameObject quitMenuButton_;
+
+	// Use this for initialization
+	void Start () {
+
+
+        //currentPanel_ = DisplayPanel.NONE;
+	}
+	
+	
+    public void ChangePanel(DisplayPanel newPanel)
+    {
+
+        //currentPanel_ = newPanel;
+
+        switch (newPanel)
+        {
+            case DisplayPanel.NONE:
+                buildingsPanel_.SetActive(false);
+                unitsPanel_.SetActive(false);
+                quitMenuButton_.SetActive(false);
+                break;
+            case DisplayPanel.BUILDINGS:
+                buildingsPanel_.SetActive(true);
+                unitsPanel_.SetActive(false);
+                quitMenuButton_.SetActive(true);
+                break;
+            case DisplayPanel.UNITS:
+                buildingsPanel_.SetActive(false);
+                unitsPanel_.SetActive(true);
+                quitMenuButton_.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ PettyKings_02/Assets/Scripts/UI Scripts/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+    private Button btn_;
+    public ConstructionDisplayManager displayManager_;
+    public DisplayPanel panel_;
+
+	// Use this for initialization
+	void Start () {
+        btn_ = GetComponent<Button>();
+        btn_.onClick.AddListener(() => displayManager_.ChangePanel(panel_));
+	}
+	
+	
+}
