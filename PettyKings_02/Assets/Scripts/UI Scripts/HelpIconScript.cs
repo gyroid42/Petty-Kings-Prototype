4c63ba0d1742054fca110c178df0b554111d0153
JakeMDuthie <jakeduthie@rocketmail.com>
2018-02-22 12:18:03 +0000

    Stage 1 UI Update
    + Help Icon Added
    + Help Icon Script
    + Forest (Tile) Prefab added for designers

---
+++ PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpIconScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Close down icon
+    public void Close(){
+        gameObject.SetActive(false);
+    }
+}
