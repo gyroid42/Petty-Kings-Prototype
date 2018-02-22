085b804708e879d8154426b70f262c9e6912cf3c
JakeMDuthie <jakeduthie@rocketmail.com>
2018-02-22 13:15:35 +0000

    Materials & Village Update
    + Removing old, unused materials from file
    + Updating current materials to look less glossy
    + Help Icon Script update

--- PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
+++ PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class HelpIconScript : MonoBehaviour {
 
+    // Find icon game object
+    public GameObject Icon;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Ensure icon is visible on startup
+        Icon.SetActive(true);
 	}
 	
 	// Update is called once per frame
@@ -16,6 +20,6 @@ public class HelpIconScript : MonoBehaviour {
 
     // Close down icon
     public void Close(){
-        gameObject.SetActive(false);
+        Icon.SetActive(false);
     }
 }
