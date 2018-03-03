dc557ddd2c26084072d786646bd7fe19ad110b19
Jock MacDonald <1501651@abertay.ac.uk>
2018-03-03 21:44:06 +0000

    added action for using new spline camera controller

---
+++ PettyKings_02/Assets/Scripts/EventSystem/EventActions/SplineCameraMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineCameraMove : BaseAction {
+
+    // Properties
+
+    public GameObject splineRoot_;
+    private SplineController cameraController;
+
+    public override void Begin(Event newEvent)
+    {
+        base.Begin(newEvent);
+
+
+        cameraController = Camera.main.GetComponent<SplineController>();
+
+        cameraController.SplineRoot = splineRoot_;
+        cameraController.FollowSpline();
+    }
+
+
+    public override void End()
+    {
+        
+    }
+
+
+    public override bool Update()
+    {
+
+        if (!cameraController.isMoving())
+        {
+            actionRunning_ = false;
+        }
+
+        return actionRunning_;
+    }
+}
