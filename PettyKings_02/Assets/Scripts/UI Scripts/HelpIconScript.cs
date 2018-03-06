d7651497a3dc2460abf6950b23869e3d3c95e271
JakeMDuthie <jakeduthie@rocketmail.com>
2018-03-06 16:14:55 +0000

    Tutorial Text Update
    + Updated ability to display text in helper
    + Multiple panels allow for clearer controls

--- PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
+++ PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelpIconScript : MonoBehaviour {
 
-    // Find icon game object
-    public GameObject Icon;
+    // Public variables
+    public GameObject icon;
+    public Text bodyObject;
+    public List<string> helperText;
+    // Private variables
+    private int listPosition = 0;
+    private int listSize;
+    
 
 	// Use this for initialization
 	void Start () {
         // Ensure icon is visible on startup
-        Icon.SetActive(true);
+        icon.SetActive(true);
+
+		// Update text
+		UpdateText();
+
+        // Get list size
+		listSize = (helperText.Count - 1);
+		Debug.Log ("List Size: " + listSize);
 	}
 	
 	// Update is called once per frame
@@ -19,7 +33,31 @@ public class HelpIconScript : MonoBehaviour {
 	}
 
     // Close down icon
-    public void Close(){
-        Icon.SetActive(false);
+    public void Close()
+    {
+        icon.SetActive(false);
+    }
+
+    void UpdateText()
+    {
+        bodyObject.text = helperText[listPosition];
+    }
+
+    // Goes to next text panel for tutorial helper, closes if the last one
+    public void CycleText()
+    {
+        // check if last item in list
+        if (listPosition == listSize)
+        {
+            // close helper
+            Close();
+        }
+        else
+        {
+            // next text
+            listPosition++;
+            // update text
+            UpdateText();
+        }
     }
 }
