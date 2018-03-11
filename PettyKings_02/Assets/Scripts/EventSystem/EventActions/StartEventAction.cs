a5f10b0ae557f78f8f1f93690515dccde05b1440
Jock MacDonald <1501651@abertay.ac.uk>
2018-03-11 21:00:02 +0000

    added a Start Event Action

---
+++ PettyKings_02/Assets/Scripts/EventSystem/EventActions/StartEventAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEventAction : BaseAction {
+
+    // properties
+    public Event event_;
+
+    private EventController eventController;
+
+    // Called at start of action
+    public override void Begin(Event newEvent)
+    {
+        base.Begin(newEvent);
+
+        eventController = EventController.eventController;
+
+        eventController.StartEvent(event_);
+        actionRunning_ = false;
+    }
+
+    // Called when action ends
+    public override void End()
+    {
+        
+    }
+
+    // Called every frame the action is active
+    public override bool Update()
+    {
+
+
+
+
+        return actionRunning_;
+    }
+}
