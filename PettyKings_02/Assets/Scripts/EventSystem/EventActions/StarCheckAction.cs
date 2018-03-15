910192922a18c0bef20ac34d3b8f68c20d175577
Jock MacDonald <1501651@abertay.ac.uk>
2018-03-15 01:15:20 +0000

    Added star check action
    Will check the star rating of the player and play a list of actions from the result

---
+++ PettyKings_02/Assets/Scripts/EventSystem/EventActions/StarCheckAction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCheckAction : BaseAction {
+
+    // Properties
+
+    // Reference to worldController to check star rating
+    private WorldManager worldController;
+
+    // Value check will be tested against
+    public int testValue_;
+
+    // Action List for each result
+    public List<BaseAction> aboveActions_;
+    public List<BaseAction> belowActions_;
+    
+    // Called at start of action
+    public override void Begin(Event newEvent)
+    {
+        base.Begin(newEvent);
+
+        // Check star rating and get action List being used
+        List<BaseAction> activeActions_ = null;
+        if (worldController.starRating >= testValue_)
+        {
+            activeActions_ = new List<BaseAction>(aboveActions_);
+        }
+        else
+        {
+            activeActions_ = new List<BaseAction>(belowActions_);
+        }
+
+        // Start that list of actions
+        currentEvent_.StartAction(activeActions_);
+
+        // End the action
+        actionRunning_ = false;
+    }
+
+
+    // Called at end of action
+    public override void End()
+    {
+        
+    }
+
+
+    // Called every frame action is active
+    public override bool Update()
+    {
+        return actionRunning_;
+    }
+}
