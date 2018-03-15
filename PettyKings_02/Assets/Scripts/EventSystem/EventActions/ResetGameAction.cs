09417a16bdeaf72c3bb2668622b132036d4762a5
Jock MacDonald <1501651@abertay.ac.uk>
2018-03-15 01:03:31 +0000

    Added actions to Reset the game and Override the curret event with a new one

---
+++ PettyKings_02/Assets/Scripts/EventSystem/EventActions/ResetGameAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGameAction : BaseAction {
+
+    // Properties
+
+
+    // Called at start of action
+    public override void Begin(Event newEvent)
+    {
+        base.Begin(newEvent);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        actionRunning_ = false;
+    }
+
+
+    // Called when action ends
+    public override void End()
+    {
+        
+    }
+
+    // Called every frame action is active
+    public override bool Update()
+    {
+        return actionRunning_;
+    }
+}
