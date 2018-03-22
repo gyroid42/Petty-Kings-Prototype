5a02736d70248edc977bf27d11e7e71ef6a2afc3
JakeMDuthie <jakeduthie@rocketmail.com>
2018-03-22 12:28:00 +0000

    Druid Helper Script Updated
    + Handles text prompts in a queue now
    + Title can now be updated
    + New texts can be added to the queue

--- PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
+++ PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
@@ -8,30 +8,45 @@ public class HelpIconScript : MonoBehaviour {
     // Public variables
     public GameObject icon;
     public Text bodyObject;
+    public Text headObject;
+
     // Set larger text input in editor
     [TextArea(1, 7)]
-    public List<string> helperText;
-    // Private variables
-    private int listPosition = 0;
-    private int listSize;
+    public List<string> tutorialText;
+
+    // Queue to handle text efficiently
+    private Queue<string> helperText;
+    private bool initialised_;
     
 
 	// Use this for initialization
 	void Start () {
         // Ensure icon is visible on startup
-        icon.SetActive(true);
+        Open();
 
-		// Update text
-		UpdateText();
-
-        // Get list size, used in CycleText for iteration
-		listSize = (helperText.Count - 1);
-	}
+        // Initialise tutorial text
+        Initialise();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
+
+    void Initialise()
+    {
+        // Create queue object
+        helperText = new Queue<string>();
+
+        // Cycle through the tutorial text list, adding each item to the queue
+        foreach(string text in tutorialText)
+        {
+            helperText.Enqueue(text);
+        }
+
+        // Update text, starting showing preloaded tutorial text
+        UpdateText();
+    }
 
     // Close down icon
     public void Close()
@@ -45,26 +60,42 @@ public class HelpIconScript : MonoBehaviour {
         icon.SetActive(true);
     }
 
-    // Update the UI box with the string stored in the list
-    // Uses list position variable to point to index in list
+    // Added a new item to the queue
+    public void AddItem(string bodyText, string title = "")
+    {
+        // Add text to the queue to be displayed
+        helperText.Enqueue(bodyText);
+
+        // Update title if new title provided
+        if (title != "")
+        {
+            UpdateTitle(title);
+        }
+    }
+
+    void UpdateTitle(string newTitle)
+    {
+        headObject.text = newTitle;
+    }
+
+    // Update the UI box with the string stored in the queue
+    // Pops top item off the top and makes the next item in the queue the top
     void UpdateText()
     {
-        bodyObject.text = helperText[listPosition];
+        bodyObject.text = helperText.Dequeue();
     }
 
     // Goes to next text panel for tutorial helper, closes if the last one
     public void CycleText()
     {
         // check if last item in list
-        if (listPosition == listSize)
+        if (helperText.Count == 0)
         {
             // close helper
             Close();
         }
         else
         {
-            // next text
-            listPosition++;
             // update text
             UpdateText();
         }
