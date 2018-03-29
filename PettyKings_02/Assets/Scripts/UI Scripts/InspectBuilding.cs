9d1859c1ae7957170cc81b6e9ae58a7efb7b6f84
JakeMDuthie <jakeduthie@rocketmail.com>
2018-03-29 11:15:18 +0100

    Bug Fix
    + Fixed bug with village buildings being selected through the UI elements
    + Still a little janky, UI elements take up much more space than the actual images

--- PettyKings_02/Assets/Scripts/UI Scripts/InspectBuilding.cs
+++ PettyKings_02/Assets/Scripts/UI Scripts/InspectBuilding.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InspectBuilding : MonoBehaviour {
 
@@ -12,20 +14,31 @@ public class InspectBuilding : MonoBehaviour {
 
     // Private Variables
     private HelpIconScript HelpIcon_;
+    private UnityEngine.EventSystems.EventSystem eventSystem_;
 
 	// Use this for initialization
 	void Awake () {
         // Locate help icon script in UI
         HelpIcon_ = GameObject.Find("ControlHelper").GetComponent<HelpIconScript>();
+
+        // Locate event system in scene for UI
+        eventSystem_ = GameObject.Find("EventSystem").GetComponent<EventSystem>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
 
     void OnMouseOver()
     {
+        // Check no UI elements are blocking
+        if(eventSystem_.IsPointerOverGameObject())
+        {
+            // Mouse pointer is over a UI element, exit function
+            return;
+        }
+        // If left mouse button is pressed, trigger function
         if(Input.GetMouseButtonDown(0))
         {
             HelpIcon_.AddItem(description, header);
