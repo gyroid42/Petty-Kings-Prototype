34216670e60c22fbd79d081b56c596d6d987efdc
Aurelie <aurelie.moiroud@gmail.com>
2018-03-30 16:18:23 +0100

    Revert "Merge branch 'master' into AurelieBranch"
    This reverts commit 93fb8201fcbe581ef467b5d7813346fb2d6a65ab, reversing
    changes made to a745ff54b01dbdeaa6f946aba04812109247c41a.

--- PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
+++ PettyKings_02/Assets/Scripts/UI Scripts/HelpIconScript.cs
@@ -17,7 +17,6 @@ public class HelpIconScript : MonoBehaviour {
     // Queue to handle text efficiently
     private Queue<HelperText> helperText;
     private bool initialised_;
-    private bool begin_;
 
     // Struct for helper data
     struct HelperText
@@ -60,26 +59,6 @@ public class HelpIconScript : MonoBehaviour {
 
 	}
 
-    void LateUpdate()
-    {
-        // only update while the timer hasn't been set to false
-        if (begin_ == false)
-        {
-            // if the control helper is open, pause it
-            if (icon.activeSelf)
-            {
-                Timer t = FindTimer();
-
-                // if a timer is present, pause it
-                if (t != null)
-                {
-                    t.Pause();
-                    begin_ = true;
-                }
-            }
-        }
-    }
-
     void Initialise()
     {
         // Create queue object
@@ -93,9 +72,6 @@ public class HelpIconScript : MonoBehaviour {
 
             helperText.Enqueue(newItem);
         }
-
-        // used for setting timer to pause later
-        begin_ = false;
 
         // Update text, starting showing preloaded tutorial text
         UpdateText();
